6137d4b5c389b3c5459cb0d16df365baec393b04
Jody Bailey <jody@jodys-mbp.attlocal.net>
2018-01-24 11:02:12 -0600

    initial commit

--- EagleSocial/ViewController.swift
+++ EagleSocial/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+       self.loginButton.layer.cornerRadius = 15
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func usernameDone() {
+    }
+    
+    @IBAction func passwordDone() {
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
