7281fd05231fcaef45a5c47ef2b0de08073ead89
LacySimpson <lacy.simpson@usm.edu>
2018-03-11 22:48:24 -0500

    Changes to the profile GUI. Group decision to not add about me or interest section at this time

--- EagleSocial/Controller/ProfileViewController.swift
+++ EagleSocial/Controller/ProfileViewController.swift
@@ -9,34 +9,17 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-    var nameFromEdit :String = ""
-    var ageFromEdit :String = ""
-    var majorFromEdit :String = ""
-    var interestFromEdit :String = ""
-    var aboutFromEdit :String = ""
-    
+class ProfileViewController: UIViewController
+{
     @IBOutlet weak var editButton: UIButton!
-    @IBOutlet weak var userNameLabel: UILabel!
-    @IBOutlet weak var userAgeLabel: UILabel!
-    @IBOutlet weak var userMajorLabel: UILabel!
-    @IBOutlet weak var userInterestLabel: UILabel!
-    @IBOutlet weak var userAboutLabel: UILabel!
     
-    
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-       // userNameLabel.text = nameFromEdit
-       // userAgeLabel.text = ageFromEdit
-       // userMajorLabel.text = majorFromEdit
-       // userInterestLabel.text = interestFromEdit
-       // userAboutLabel.text = aboutFromEdit
-        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
