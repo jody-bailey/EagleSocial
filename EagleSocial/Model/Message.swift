200fc97911c0a879986e914f1205deb6f4d2168d
Jason <michaeljpearson1980@gmail.com>
2018-03-26 23:02:00 -0500

    3/27/2018 Fixed an issue with the tapGesterRecognizer so the function to dismiss the keyboard would work. Rounded the corners on the message bubbles so the message list looks more like a messaging screen. Added a generic profile image to each message retrieved at least until the profiles are built out complete with profile picture that I can pull from. Removed the separator so there will be no lines in between the messages. Migrated and refactored some of the firebase code for sending new messages away from the view controller and put it in the model. Added more comments and spacing for improved code readability.
    Class Message
    Made all the attributes private
    Added 3 new attributes: conversationID, messageDictionary, and members
    Added initializer function and overloaded initializer function.
    Added sendMethod method.
    Added getters for SenderId, messageBody, conversationID, messageDictionary, and members.
    Added setters for SenderId, messageBody, and messageDictionary.

--- EagleSocial/Model/Message.swift
+++ EagleSocial/Model/Message.swift
@@ -7,10 +7,129 @@
 //
 
 import Foundation
+import Firebase
 
 class Message {
     
-    var senderId : String = ""
-    var messageBody : String = ""
+    private var senderId : String
+    private var messageBody : String
+    private var conversationID : String
+    private var messageDictionary : Dictionary<String, Any>
+    private var members : Dictionary<String,Bool>
+    
+    init() {
+        senderId = ""
+        messageBody = ""
+        conversationID = ""
         
+        messageDictionary = ["Sender" : "a", "MessageBody" : "b", "ConversationID": "c"]
+        members = ["abc": true, "123": true]
+    }
+    init(membersa: Dictionary<String, Bool>, messageDictionarya: Dictionary<String, Any>)
+    {
+        senderId = String(describing: messageDictionarya["Sender"]!)
+        messageBody = String(describing: messageDictionarya["MessageBody"]!)
+        conversationID = String(describing: messageDictionarya["ConversationID"]!)
+        
+        messageDictionary = messageDictionarya
+        members = membersa
+    }
+   
+    //MARK: - Send Messages
+    func sendMessage()
+    {
+        let conversationDB = Database.database().reference().child("Conversation")
+        //let messagesDB = conversationDB.child("Messages")
+        //conversationID = String(describing: messageDictionary["ConversationID"])
+        //conversationID = conversationIDb
+        
+        //If this is a new conversation
+        if conversationID == "Optional(\"\")" {
+            conversationID = conversationDB.childByAutoId().key
+            messageDictionary["ConversationID"] = conversationID
+            conversationDB.child(conversationID).updateChildValues(["Members" : members]) {
+                (error, reference) in
+                
+                
+                if error != nil {
+                    print(error!)
+                    
+                } else {
+                    conversationDB.child(self.conversationID).child("Messages").childByAutoId().updateChildValues(self.messageDictionary)
+                    
+                    print ("Message saved successfully!!")
+                }
+            }
+        }
+        //If this is an existing conversation
+        else  {
+            conversationDB.child(conversationID).child("Messages").childByAutoId().updateChildValues(messageDictionary) {
+                (error, reference) in
+                
+                if error != nil {
+                    print(error!)
+                    
+                } else {
+                                        
+                    print ("Message saved successfully!!")
+                }
+            }
+        }
+        
+    }
+    
+    //MARK: - Receive Messages
+    //TODO: - Refactor to use the class to retrieve messages from the database.
+    
+   /* func retrieveMessages(conversation: String) -> Message {
+        
+        //let conversationDB = Database.database().reference().child("Conversation")
+            
+            //Members/CurrentUserID
+            //.queryOrdered(byChild: "Members/" + (Auth.auth().currentUser?.uid)!)
+            //.queryEqual(toValue: true)
+        let messageDB = Database.database().reference().child("Conversation").child(conversation).child("Messages")
+        let message1 = Message()
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String,Any>
+            
+            
+            message1.setMessageBody(messageBod: String(describing: snapshotValue["MessageBody"]!))
+            message1.setSenderId(sender: String(describing: snapshotValue["Sender"]!))
+            message1.conversationID = conversation
+            message1.setMessageDictionary(messageDict:  ["Sender": snapshotValue["Sender"]!,
+                                      "MessageBody": snapshotValue["MessageBody"]!,
+                                      "ConversationID" : conversation])
+        }
+        return message1
+    }*/
+    
+    //MARK: - Getters
+    func getSenderId() -> String {
+        return senderId
+    }
+    func getMessageBody() -> String {
+        return messageBody
+    }
+    func getConversationID() -> String {
+        return conversationID
+    }
+    func getMessageDictionary() -> Dictionary <String, Any> {
+        return messageDictionary
+    }
+    func getMembers() -> Dictionary <String, Bool> {
+        return members
+    }
+    
+    //MARK: - Setters
+    func setSenderId(sender: String) {
+        senderId = sender
+    }
+    func setMessageBody(messageBod: String) {
+        messageBody = messageBod
+    }
+    func setMessageDictionary(messageDict: Dictionary<String, Any>) {
+        messageDictionary = messageDict
+    }
 }
