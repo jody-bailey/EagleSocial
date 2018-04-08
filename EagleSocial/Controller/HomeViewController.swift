44a8141835bfc5aca54cedcd0a26a24f615c21e8
LacySimpson <lacy.simpson@usm.edu>
2018-04-08 00:53:11 -0500

    Cosmetic changes made to the Profile View Controller

--- EagleSocial/Controller/HomeViewController.swift
+++ EagleSocial/Controller/HomeViewController.swift
@@ -157,7 +157,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             if (self.posts[indexPath.row - 1].userId == thisUser.userID){
                 cell.profilePicture.image = thisUser.profilePic
             } else {
-                cell.profilePicture.image = friendList.getFriend(userId: self.posts[indexPath.row - 1].userId).profilePic
+//                cell.profilePicture.image = friendList.getFriend(userId: self.posts[indexPath.row - 1].userId).profilePic
 //                cell.profilePicture.image = #imageLiteral(resourceName: "profile_icon")
             }
             cell.profilePicture.layer.cornerRadius = 10
