4da153fd2299e33de3f12ec791eb025bd68f4d37
Jody Bailey <jodybailey93@gmail.com>
2018-04-23 14:42:31 -0500

    got the comments to display in the order that they were posted.

---
+++ EagleSocial/Controller/ForgotPasswordController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgotPasswordController.swift
+//  EagleSocial
+//
+//  Created by Jody Bailey on 4/18/18.
+//  Copyright © 2018 Jody Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
